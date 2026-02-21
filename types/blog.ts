747219b8ff88572fc94be471805d9f915fb18977
ratulislam46 <arfanratul46@gmail.com>
2026-02-21 13:40:14 +0600

    Added blogs data

---
+++ types/blog.ts
@@ -0,0 +1,13 @@
+export interface BlogPost {
+  id: number;
+  title: string;
+  excerpt: string;
+  author: string;
+  date: string;
+  readTime: string;
+  category: string;
+  image: string;
+  views: string;
+  comments: number;
+  slug: string;
+}
